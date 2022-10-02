c0f5ed3dc70d3d4db779797f2f2fa25d9e89c0af
Linos Nchena <linosnchena@gmail.om>
2022-10-02 19:25:19 +0200

    delete and insert chained

--- 1spring-boot-security-pgSql/src/main/resources/data.sql
+++ 1spring-boot-security-pgSql/src/main/resources/data.sql
@@ -1,3 +1,6 @@
+delete from user_roles;
+delete from rolesx;
+
 INSERT INTO rolesx(name) VALUES('ROLE_USER');
 INSERT INTO rolesx(name) VALUES('ROLE_MODERATOR');
 INSERT INTO rolesx(name) VALUES('ROLE_ADMIN');
